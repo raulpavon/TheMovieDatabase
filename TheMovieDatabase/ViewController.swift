9b2deb1c140ae2b7257b3ccfc68a73094a045266
Raúl Leonardo Pavón Toral <rpavon@novasolutionsystems.com>
2021-12-06 16:58:31 -0600

    Initial Commit

---
+++ TheMovieDatabase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDatabase
+//
+//  Created by Raúl Leonardo Pavón Toral on 06/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
